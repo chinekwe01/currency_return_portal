bb8fe6dfd52ef47f0b84e466b22677c7e0c8e19c
chinekwe01 <chinekwe_clever@outlook.com>
2023-07-10 08:22:53 +0100

    added link for going back to homepage

--- resources/views/return-currency.blade.php
+++ resources/views/return-currency.blade.php
@@ -58,7 +58,12 @@
 
     <section class="bg-white dark:bg-gray-900">
         <div class="py-8 px-4 mx-auto max-w-2xl lg:py-16">
-            <h2 class="mb-4 text-xl font-bold text-gray-900 dark:text-white">Data Entry</h2>
+            <div class="flex justify-between">
+                    <h2 class="mb-4 text-xl font-bold text-gray-900 dark:text-white">Data Entry</h2>
+                    <button class="bg-blue-400 hover:bg-blue-600 text-white py-1 px-2 rounded">
+                        <a href="/">Go back</a>
+                    </button>
+            </div>
             <form action="{{ route('return-currency.store') }}" method="POST">
                 @csrf
                 <div class="grid gap-4 sm:grid-cols-2 sm:gap-6">
